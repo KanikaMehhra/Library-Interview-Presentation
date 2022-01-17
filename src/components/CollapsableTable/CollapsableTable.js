b47c4bb14fd8df594e7e63176a3d4718835f7c12
Kanika Mehra <kanmehra@deloitte.com.au>
2022-01-17 20:55:36 +1100

    coloured header added

--- src/components/CollapsableTable/CollapsableTable.js
+++ src/components/CollapsableTable/CollapsableTable.js
@@ -1,15 +1,31 @@
-import * as React from 'react';
-import { TableRow, Table, TableBody, TableCell, TableContainer, Paper, TableHead } from '@material-ui/core'
-import ExpandableRow from '../ExpandableRow/ExpandableRow';
+
+
+import React from "react";
+import {  withStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import MuiTableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import ExpandableRow from "../ExpandableRow/ExpandableRow";
+
+const TableHead = withStyles((theme) => ({
+    root: {
+        backgroundColor: "orange"
+    }
+}))(MuiTableHead);
 
 const CollapsableTable = ({ rows }) => {
+
     return (
         <TableContainer component={Paper}>
-            <Table stickyHeader aria-label="collapsible table">
+            <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
                         <TableCell />
-                        <TableCell>Title</TableCell>
+                        <TableCell align="left">Title</TableCell>
                         <TableCell align="right">Author</TableCell>
                     </TableRow>
                 </TableHead>
@@ -24,3 +40,4 @@ const CollapsableTable = ({ rows }) => {
 }
 
 export default CollapsableTable;
+
